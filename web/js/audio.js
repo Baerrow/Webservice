9727489b58ac2207d28c04057ae92916fee6eb37
Nivii <beatrice.djearamane@epsi.fr>
2015-10-21 22:48:40 +0200

    Audio fini, et ajout des fichiers js et du beau css

---
+++ web/js/audio.js
@@ -0,0 +1,30 @@
+
+	$(document).ready(function()
+	{
+        $('#datable').DataTable();
+    });
+
+    var player = document.getElementById('player');
+    function changeSecToMin(seconds) 
+    {
+        sec = Math.floor( seconds );    
+        min = Math.floor( sec / 60 );
+        min = min >= 10 ? min : '0' + min;    
+        sec = Math.floor( sec % 60 );
+        sec = sec >= 10 ? sec : '0' + sec;    
+        return min + ':' + sec;
+    }
+
+    function stopAudio()
+    {
+        player = document.getElementById('player');
+        player.pause();
+        player.currentTime = 0;
+    }
+
+    function goBack()
+    {
+        player = document.getElementById('player');
+        player.pause();
+        player.currentTime = player.currentTime - 20;
+    }
